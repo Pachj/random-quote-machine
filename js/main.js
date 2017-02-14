73a2031ccd7cea5adb30a5ffb36771808e940e82
Pachj <Pachj@users.noreply.github.com>
2017-02-14 15:30:56 +0100

    main.js refactored

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@
  * Created by Henry on 10.02.17.
  */
 $(document).ready(function () {
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-        $("#quote").html(a[0].content);
-        $("#footer").html(a[0].title);
-        $.ajax({
-            cache : false,
-            //origin : "https://pachj.github.io/random-quote-machine/"
-        });
+    $.ajaxSetup({
+        cache : false
+        //origin : "https://pachj.github.io/random-quote-machine/"
     });
 
-    $("#new-quote").on("click", function () {
-        $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-            $("#quote").html(a[0].content);
-            $("#footer").html(a[0].title);
-            $.ajax({
-                cache : false,
-                //origin : "https://pachj.github.io/random-quote-machine/"
-            });
-        });
+    getQuote();
+
+    $("#new-quote").click(function () {
+        getQuote();
     });
+
+    function getQuote() {
+        $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(quote) {
+            $("#quote").html(quote[0].content);
+            $("#footer").html(quote[0].title);
+        });
+    }
 });
+
+
 
