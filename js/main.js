955dcbed260a3f0c6c93fa4068a41453121ed4c9
Pachj <Pachj@users.noreply.github.com>
2017-02-14 14:42:00 +0100

    test number 2 to fix Control-Origin error

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Created by Henry on 10.02.17.
  */
 $(document).ready(function () {
-    $.getJSON("https://quotesondesign.com/wp-json/posts?origin=https://quotesondesign.com/wp-json/posts&filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
         $("#quote").html(a[0].content);
         $("#footer").html(a[0].title);
         $.ajaxSetup({
@@ -12,7 +12,7 @@ $(document).ready(function () {
     });
 
     $("#new-quote").on("click", function () {
-        $.getJSON("https://quotesondesign.com/wp-json/posts?origin=https://quotesondesign.com/wp-json/posts&filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+        $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
             $("#quote").html(a[0].content);
             $("#footer").html(a[0].title);
             $.ajaxSetup({
