2f41548693079763209ffc438f547a8f1b908cdd
Pachj <Pachj@users.noreply.github.com>
2017-02-14 19:22:54 +0100

    many, many, many changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function () {
     $.ajaxSetup({
-        cache : false
+        cache: false
         //origin : "https://pachj.github.io/random-quote-machine/"
     });
 
@@ -11,13 +11,26 @@ $(document).ready(function () {
 
     $("#new-quote").click(function () {
         getQuote();
+        getRandomColor();
     });
 
     function getQuote() {
-        $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(quote) {
-            $("#quote").html(quote[0].content);
-            $("#footer").html(quote[0].title);
+        $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function (quote) {
+            if ((quote[0].content.length + quote[0].title.length) < 140) {
+                $("#quote").html(quote[0].content);
+                $("#footer").html(quote[0].title);
+            }
+            else {
+                getQuote();
+            }
         });
+    }
+
+    function getRandomColor() {
+        var colors = ["#29BF12", "#09B2CB", "#F21B3F", "#C4008C"];
+        var rand = colors[Math.floor(Math.random() * colors.length)];
+        $("#action-box").css("background-color", rand);
+        document.body.style.setProperty('--dot-color', rand);
     }
 });
 
