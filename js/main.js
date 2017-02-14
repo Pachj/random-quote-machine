bef8ed7f8d789a49c129c3524703de15436ff91e
Pachj <Pachj@users.noreply.github.com>
2017-02-14 14:19:08 +0100

    gets now a random quote from the api

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@
  * Created by Henry on 10.02.17.
  */
 $(document).ready(function () {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?origin=http://quotesondesign.com/wp-json/posts&filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+        $("#quote").html(a[0].content);
+        $("#footer").html(a[0].title);
+        $.ajaxSetup({
+            cache : false
+        });
+    });
 
+    $("#new-quote").on("click", function () {
+        $.getJSON("http://quotesondesign.com/wp-json/posts?origin=http://quotesondesign.com/wp-json/posts&filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+            $("#quote").html(a[0].content);
+            $("#footer").html(a[0].title);
+            $.ajaxSetup({
+                cache : false
+            });
+        });
+    });
 });
 
