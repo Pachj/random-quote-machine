42f298804092727286227f606a231d3cf2647a3c
Pachj <Pachj@users.noreply.github.com>
2017-02-14 14:24:11 +0100

    gets the quote from the api now over HTTPS (now all)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Created by Henry on 10.02.17.
  */
 $(document).ready(function () {
-    $.getJSON("http://quotesondesign.com/wp-json/posts?origin=http://quotesondesign.com/wp-json/posts&filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    $.getJSON("https://quotesondesign.com/wp-json/posts?origin=https://quotesondesign.com/wp-json/posts&filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
         $("#quote").html(a[0].content);
         $("#footer").html(a[0].title);
         $.ajaxSetup({
