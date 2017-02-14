c9f22301f1b85283705e39e299f168111c135782
Pachj <Pachj@users.noreply.github.com>
2017-02-14 15:04:20 +0100

    test number 3 to fix Control-Origin error

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
     $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
         $("#quote").html(a[0].content);
         $("#footer").html(a[0].title);
-        $.ajaxSetup({
+        $.ajax({
             cache : false,
-            origin : "https://pachj.github.io/random-quote-machine/"
+            //origin : "https://pachj.github.io/random-quote-machine/"
         });
     });
 
@@ -15,9 +15,9 @@ $(document).ready(function () {
         $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
             $("#quote").html(a[0].content);
             $("#footer").html(a[0].title);
-            $.ajaxSetup({
+            $.ajax({
                 cache : false,
-                origin : "https://pachj.github.io/random-quote-machine/"
+                //origin : "https://pachj.github.io/random-quote-machine/"
             });
         });
     });
